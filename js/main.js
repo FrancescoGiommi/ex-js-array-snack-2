1d35ad49dcabf89c962ca55ff7b0820ddbd10cbb
Francesco <you@example.com>
2025-03-04 15:29:05 +0100

    Add snack 1

--- js/main.js
+++ js/main.js
@@ -78,8 +78,22 @@ const books = [
 /* Crea una funzione che somma due numeri.
 
     Crea un array (longBooks) con i libri che hanno più di 300 pagine;
+
     Creare un array (longBooksTitles) che contiene solo i titoli dei libri contenuti in longBooks.
     Stampa in console ogni titolo nella console. */
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+const somma = sum(5, 5);
+console.log(somma);
+
+const longBooks = books.filter((book) => book.pages > 300);
+console.log(longBooks);
+
+const longBooksTitles = longBooks.map((book) => book.title);
+console.log(longBooksTitles);
+console.log(longBooksTitles[0], longBooksTitles[1]);
 
 //! Snack 2 - Il primo libro scontato
 
