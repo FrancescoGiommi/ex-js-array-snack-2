496e84f3f88a6f50245a4ada212adb0e4fbed465
Francesco <you@example.com>
2025-03-07 22:49:11 +0100

    Add snack 6

--- js/main.js
+++ js/main.js
@@ -172,3 +172,17 @@ const getBooks = async (ids) => {
 getBooks(ids)
   .then((books) => console.log(books))
   .catch((error) => console.log(error));
+
+//! Snack 6 (Bonus) - Ordina i libri
+
+/* Crea una variabile booleana (areThereAvailableBooks) per verificare se c’è almeno un libro disponibile.
+Crea un array (booksByPrice) con gli elementi di books ordinati in base al prezzo (crescente).
+Ordina l’array booksByPricein base alla disponibilità (prima quelli disponibili), senza creare un nuovo array.*/
+
+const booksByPrice = [...books].sort((a, b) => {
+  const priceA = Number(a.price.replace("€", ""));
+  const priceB = Number(b.price.replace("€", ""));
+  return priceA - priceB;
+});
+
+console.log(booksByPrice);
